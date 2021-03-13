dfc9c9c7182a7b0be921565a4ba69ea43b99922f
Selt <fightggw@gmail.com>
2021-03-13 19:18:24 +0800

    订单管理

---
+++ src/main/java/com/how2java/tmall/controller/OrderController.java
@@ -0,0 +1,63 @@
+package com.how2java.tmall.controller;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.how2java.tmall.pojo.Order;
+import com.how2java.tmall.service.OrderItemService;
+import com.how2java.tmall.service.OrderService;
+import com.how2java.tmall.util.Page;
+
+@Controller
+@RequestMapping("")
+public class OrderController {
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderItemService orderItemService;
+
+    @RequestMapping("admin_order_list")
+    public String list(Model model, Page page){
+        //获取分页对象
+        PageHelper.offsetPage(page.getStart(),page.getCount());
+
+        //查询订单集合
+        List<Order> os= orderService.list();
+
+        //获取订单总数并设置在分页对象上
+        int total = (int) new PageInfo<>(os).getTotal();
+        page.setTotal(total);
+
+        //借助orderItemService.fill()方法为这些订单填充上orderItems信息
+        orderItemService.fill(os);
+
+        //把订单集合和分页对象设置在model上
+        model.addAttribute("os", os);
+        model.addAttribute("page", page);
+
+        //服务端跳转到admin/listOrder.jsp页面
+        return "admin/listOrder";
+    }
+
+    //发货按钮链接跳转到admin_order_delivery
+    @RequestMapping("admin_order_delivery")
+    //注入订单对象
+    public String delivery(Order o) throws IOException {
+        //修改发货时间，设置发货状态
+        o.setDeliveryDate(new Date());
+        o.setStatus(OrderService.waitConfirm);
+
+        //更新到数据库
+        orderService.update(o);
+        //客户端跳转到admin_order_list页面
+        return "redirect:admin_order_list";
+    }
+}
---
+++ src/main/java/com/how2java/tmall/service/impl/OrderItemServiceImpl.java
@@ -0,0 +1,95 @@
+package com.how2java.tmall.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.how2java.tmall.mapper.OrderItemMapper;
+import com.how2java.tmall.pojo.Order;
+import com.how2java.tmall.pojo.OrderItem;
+import com.how2java.tmall.pojo.OrderItemExample;
+import com.how2java.tmall.pojo.Product;
+import com.how2java.tmall.service.OrderItemService;
+import com.how2java.tmall.service.ProductService;
+
+@Service
+public class OrderItemServiceImpl implements OrderItemService {
+    @Autowired
+    OrderItemMapper orderItemMapper;
+    @Autowired
+    ProductService productService;
+
+    @Override
+    public void add(OrderItem c) {
+        orderItemMapper.insert(c);
+    }
+
+    @Override
+    public void delete(int id) {
+        orderItemMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(OrderItem c) {
+        orderItemMapper.updateByPrimaryKeySelective(c);
+    }
+
+    @Override
+    public OrderItem get(int id) {
+        OrderItem result = orderItemMapper.selectByPrimaryKey(id);
+        setProduct(result);
+        return result;
+    }
+
+    public List<OrderItem> list(){
+        OrderItemExample example =new OrderItemExample();
+        example.setOrderByClause("id desc");
+        return orderItemMapper.selectByExample(example);
+
+    }
+
+    @Override
+    public void fill(List<Order> os) {
+        for (Order o : os) {
+            fill(o);
+        }
+    }
+
+    public void fill(Order o) {
+        OrderItemExample example =new OrderItemExample();
+        //根据订单id查询出其对应的所有订单项
+        example.createCriteria().andOidEqualTo(o.getId());
+        example.setOrderByClause("id desc");
+        List<OrderItem> ois =orderItemMapper.selectByExample(example);
+        //通过setProduct为所有的订单项设置Product属性
+        setProduct(ois);
+
+        float total = 0;
+        int totalNumber = 0;
+        //遍历所有的订单项，然后计算出该订单的总金额和总数量
+        for (OrderItem oi : ois) {
+            total+=oi.getNumber()*oi.getProduct().getPromotePrice();
+            totalNumber+=oi.getNumber();
+        }
+        o.setTotal(total);
+        o.setTotalNumber(totalNumber);
+        //最后再把订单项设置在订单的orderItems属性上。
+        o.setOrderItems(ois);
+
+    }
+
+    public void setProduct(List<OrderItem> ois){
+        for (OrderItem oi: ois) {
+            setProduct(oi);
+        }
+    }
+
+    private void setProduct(OrderItem oi) {
+        Product p = productService.get(oi.getPid());
+        oi.setProduct(p);
+    }
+
+    ;
+
+}
